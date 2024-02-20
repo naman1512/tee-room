64c90e27331593d7a1bbc334b3ef9da363191f23
naman1512 <141447023+naman1512@users.noreply.github.com>
2024-02-20 14:33:31 +0530

    updated backend

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -10,15 +10,16 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+
 router.route('/').get((req, res) => {
-  res.status(200).json({ message: "Hello from DALL.E ROUTES" });
-});
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" })
+})
 
 router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const response = await openai.images.create({
+    const response = await openai.createImage({
       prompt,
       n: 1,
       size: '1024x1024',
@@ -29,9 +30,9 @@ router.route('/').post(async (req, res) => {
 
     res.status(200).json({ photo: image });
   } catch (error) {
-    console.error("OpenAI API Error:", error);
-    res.status(500).json({ message: "Something went wrong" });
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" })
   }
-});
+})
 
 export default router;
