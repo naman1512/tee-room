40c1e5175937f2532c2ac717e91ccd497b5ba754
naman1512 <141447023+naman1512@users.noreply.github.com>
2024-02-20 07:03:33 +0530

    Update dalle.route.js

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -29,7 +29,7 @@ router.route('/').post(async (req, res) => {
 
     res.status(200).json({ photo: image });
   } catch (error) {
-    console.error(error);
+    console.error("OpenAI API Error:", error);
     res.status(500).json({ message: "Something went wrong" });
   }
 });
