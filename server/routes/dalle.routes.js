9eb8778bd94b92f3ac7fd6aa0d9ff6f10912c4be
naman1512 <141447023+naman1512@users.noreply.github.com>
2024-02-21 21:32:05 +0530

    Update dalle.routes.js

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,8 +1,13 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
-import OpenAI  from 'openai';
+import { config as dotenvConfig } from 'dotenv';
+import OpenAI from 'openai';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 
-dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+dotenvConfig({ path: resolve(__dirname, '../../.env') });
 
 const router = express.Router();
 
@@ -10,10 +15,9 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-
 router.route('/').get((req, res) => {
-  res.status(200).json({ message: "Hello from DALL.E ROUTES" })
-})
+  res.status(200).json({ message: 'Hello from DALL.E ROUTES' });
+});
 
 router.route('/').post(async (req, res) => {
   try {
@@ -23,17 +27,16 @@ router.route('/').post(async (req, res) => {
       prompt,
       n: 1,
       size: '1024x1024',
-      return_format: 'url'
+      return_format: 'url',
     });
-    
 
     const image = response.data.data[0].b64_json;
 
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Something went wrong" })
+    res.status(500).json({ message: 'Something went wrong' });
   }
-})
+});
 
 export default router;
